3735ac743ab2b45ffdc806f71bf6e72487721187
Pixel development <tosterqgg@gmail.com>
2025-12-31 19:35:42 +0100

    Update ui.sh

--- ui.sh
+++ ui.sh
@@ -10,84 +10,76 @@ fi
 # Załaduj funkcje
 source "$(dirname "$0")/functions.sh"
 
-# Uruchom sprawdzanie zależności
-! command -v tput &> /dev/null && check_dependencies
+# Sprawdź zależności
+command -v tput &> /dev/null || check_dependencies
 
-# Główna pętla
+# ------------------ Główna pętla menu ------------------
 main() {
-    local selected=0 max_options=7 old_stty=$(stty -g)
+    local selected=0
+    local max_options=7
+    local old_stty=$(stty -g)
+
+    # Włącz tryb myszy
     printf '\033[?1000h\033[?1002h\033[?1006h'
-    
+
     while true; do
         show_menu $selected
         IFS= read -rsn1 key
-        
+
         if [[ $key == $'\x1b' ]]; then
+            # Obsługa klawiszy specjalnych i myszy
             IFS= read -rsn1 -t 0.01 key2
             if [[ $key2 == '[' ]]; then
                 IFS= read -rsn1 -t 0.01 key3
-                if [[ $key3 == 'A' ]]; then
-                    ((selected--))
-                    [ $selected -lt 0 ] && selected=$max_options
-                elif [[ $key3 == 'B' ]]; then
-                    ((selected++))
-                    [ $selected -gt $max_options ] && selected=0
-                elif [[ $key3 == '<' ]]; then
-                    local mouse_seq="<"
-                    while IFS= read -rsn1 -t 0.01 char; do
-                        mouse_seq+="$char"
-                        [[ $char == 'M' || $char == 'm' ]] && break
-                    done
-                    
-                    if [[ "$mouse_seq" =~ \<([0-9]+)\;([0-9]+)\;([0-9]+)M ]]; then
-                        local button="${BASH_REMATCH[1]}"
-                        local x="${BASH_REMATCH[2]}"
-                        local y="${BASH_REMATCH[3]}"
-                        local clicked=$((y - 7))
-                        
-                        if [ $clicked -ge 0 ] && [ $clicked -le $max_options ]; then
-                            selected=$clicked
-                            case $selected in
-                                0) install_package ;;
-                                1) uninstall_package ;;
-                                2) update_system ;;
-                                3) custom_command ;;
-                                4) install_de ;;
-                                5) helpful_commands ;;
-                                6) select_package_manager ;;
-                                7)
-                                    clear
-                                    printf '\033[?1000l\033[?1002l\033[?1006l'
-                                    info "Do widzenia!"
-                                    stty "$old_stty"
-                                    exit 0
-                                    ;;
-                            esac
+                case "$key3" in
+                    A) ((selected--)); [ $selected -lt 0 ] && selected=$max_options ;;
+                    B) ((selected++)); [ $selected -gt $max_options ] && selected=0 ;;
+                    '<')
+                        # Obsługa myszy
+                        local mouse_seq="<"
+                        while IFS= read -rsn1 -t 0.01 char; do
+                            mouse_seq+="$char"
+                            [[ $char == 'M' || $char == 'm' ]] && break
+                        done
+                        if [[ "$mouse_seq" =~ \<([0-9]+)\;([0-9]+)\;([0-9]+)M ]]; then
+                            local clicked=$((BASH_REMATCH[2]-7))
+                            if [ $clicked -ge 0 ] && [ $clicked -le $max_options ]; then
+                                selected=$clicked
+                                handle_selection $selected old_stty
+                            fi
                         fi
-                    fi
-                fi
+                        ;;
+                esac
             fi
         elif [[ $key == "" ]]; then
-            case $selected in
-                0) install_package ;;
-                1) uninstall_package ;;
-                2) update_system ;;
-                3) custom_command ;;
-                4) install_de ;;
-                5) helpful_commands ;;
-                6) select_package_manager ;;
-                7)
-                    clear
-                    printf '\033[?1000l\033[?1002l\033[?1006l'
-                    info "Do widzenia!"
-                    stty "$old_stty"
-                    exit 0
-                    ;;
-            esac
+            handle_selection $selected old_stty
         fi
     done
 }
 
-# Start
+# ------------------ Obsługa wyboru ------------------
+handle_selection() {
+    local sel=$1
+    local old_stty=$2
+    case $sel in
+        0) install_package ;;
+        1) uninstall_package ;;
+        2) update_system ;;
+        3) custom_command ;;
+        4) install_de ;;
+        5) helpful_commands ;;
+        6) select_package_manager ;;
+        7)
+            clear
+            printf '\033[?1000l\033[?1002l\033[?1006l'
+            info "Do widzenia!"
+            stty "$old_stty"
+            exit 0
+            ;;
+    esac
+}
+
+# ------------------ Start ------------------
+# Wybór menedżera pakietów przed wejściem do menu
 select_package_manager
 main
